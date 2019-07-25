15e802ef2f4fe38b9c23b9aebae0058eeed51a8d
Paritosh <paritosh.yadav.237@gmail.com>
2019-07-25 18:08:51 +0530

    Bottom tab navigations added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,11 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import React from "react";
+import { Image } from "react-native";
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from "react-navigation";
 import { HomeScreen, DetailScreen, ModalScreen, SettingsScreen, ProfileScreen } from "component";
+import { home, settings } from "image";
 
-const AppNavigator = createStackNavigator(
-    {
-        Home: HomeScreen,
-        Details: DetailScreen,
-        Settings: SettingsScreen,
-        Profile: ProfileScreen,
-    },
-    {
-        initialRouteName: "Home",
+const defaultNavigationOptions = {
+    stackNavigator: {
         defaultNavigationOptions: {
             headerStyle: {
                 backgroundColor: "#f4511e",
@@ -19,14 +15,76 @@ const AppNavigator = createStackNavigator(
             headerTitleStyle: {
                 fontWeight: "bold",
             },
+        }
+    },
+    tabNavigator: {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === "Home") {
+                    iconName = home;
+                } else if (routeName === "Settings") {
+                    iconName = settings;
+                }
+                // You can return any component that you like here!
+                return <Image source={iconName} style={{ width: 20, height: 20, tintColor }} resizeMode="contain" />;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
         },
+    }
+};
+
+const HomeStack = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Details: DetailScreen,
+        Profile: ProfileScreen,
+    },
+    {
+        ...defaultNavigationOptions.stackNavigator,
+    }
+);
+
+HomeStack.navigationOptions = ({ navigation }) => {
+    let tabBarVisible = true;
+    if (navigation.state.index === 1) {
+        tabBarVisible = false;
+    }
+
+    return {
+        tabBarVisible,
+    };
+};
+
+const SettingsStack = createStackNavigator(
+    {
+        Settings: SettingsScreen,
+        Profile: ProfileScreen,
+    },
+    {
+        ...defaultNavigationOptions.stackNavigator,
+    }
+);
+
+const TabNavigator = createBottomTabNavigator(
+    {
+        Home: HomeStack,
+        Settings: SettingsStack,
+    },
+    {
+        initialRouteName: "Settings",
+        ...defaultNavigationOptions.tabNavigator,
     }
 );
 
 const RootNavigator = createStackNavigator(
     {
-        App: {
-            screen: AppNavigator,
+        Tabs: {
+            screen: TabNavigator,
         },
         MyModal: {
             screen: ModalScreen,
