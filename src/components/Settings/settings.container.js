b60078162d28efc1ee1ec79cfc688318768725f0
Paritosh <paritosh.yadav.237@gmail.com>
2019-07-25 15:22:49 +0530

    back navigation with 'naviagte' function

---
+++ src/components/Settings/settings.container.js
@@ -0,0 +1,85 @@
+
+import React, { Fragment } from "react";
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Button,
+} from "react-native";
+
+import {
+    Header,
+    Colors,
+} from "react-native/Libraries/NewAppScreen";
+
+export default class SettingsScreen extends React.Component {
+    static navigationOptions = {
+        title: "Settings",
+    };
+    render() {
+        return (
+            <Fragment>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView>
+                    <ScrollView
+                        contentInsetAdjustmentBehavior="automatic"
+                        style={styles.scrollView}>
+                        <Header />
+                        <View style={styles.body}>
+                            <Button
+                                title="Go to Details"
+                                onPress={() => this.props.navigation.navigate("Details")} // Navigate back to 'Details' clearing all the stack
+                            // onPress={() => this.props.navigation.push("Details")} // push 'Details' again on the stack maintaining back stack
+                            />
+                            <View style={styles.sectionContainer}>
+                                <Text style={styles.sectionTitle}>Settings Screen</Text>
+                            </View>
+                        </View>
+                    </ScrollView>
+                </SafeAreaView>
+            </Fragment>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    scrollView: {
+        backgroundColor: Colors.lighter,
+    },
+    engine: {
+        position: "absolute",
+        right: 0,
+    },
+    body: {
+        backgroundColor: Colors.white,
+    },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: "600",
+        color: Colors.black,
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: "400",
+        color: Colors.dark,
+    },
+    highlight: {
+        fontWeight: "700",
+    },
+    footer: {
+        color: Colors.dark,
+        fontSize: 12,
+        fontWeight: "600",
+        padding: 4,
+        paddingRight: 12,
+        textAlign: "right",
+    },
+});
