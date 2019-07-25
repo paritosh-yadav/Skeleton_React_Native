4a679dacbc42de6fc1f04649f6f825e4320c33c2
Paritosh <paritosh.yadav.237@gmail.com>
2019-07-25 18:07:57 +0530

    icons added for tabbar

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,6 @@
 import logo from "./logo.png";
 import backIcon from "./backicon.png";
+import home from "./home.png";
+import settings from "./settings.png";
 
-export { logo, backIcon };
+export { logo, backIcon, home, settings };
